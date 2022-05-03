f895bfee9ca64dd1b12081d19418dbbe26c14969
Yeshu Deshmukh <89732654+yeshudeshmukh@users.noreply.github.com>
2022-05-03 19:55:13 +0530

    Project uploaf

---
+++ neevati/js/owl.js
@@ -0,0 +1,29 @@
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:20,
+//     autoplay:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:1
+//         },
+//         1000:{
+//             items:1
+//         }
+//     }
+// })
+
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items:1,
+    loop:true,
+    autoplay:true,
+    margin:30,
+    responsive:{
+        600:{
+            items:2
+        }
+    }
+});
